3707e60839c6004c4619d80c7c6de886b4e2558e
Kobus Viljoen <kobusv@gmail.com>
2018-12-03 14:17:09 +0200

    Solutions for day3

---
+++ day03/solution.js
@@ -0,0 +1,62 @@
+function* range(start, end, step = 1) {
+  for (let i = start; i < end; i += step) {
+    yield i;
+  }
+}
+
+const parseFile = input =>
+  input.split('\n').map(x => {
+    const split = x.replace(/[@:#]/g, '').split(' ');
+    return [split[0], ...split[2].split(','), ...split[3].split('x')].map(y =>
+      parseInt(y, 10),
+    );
+  });
+
+const part1 = input => {
+  const claims = parseFile(input);
+  claims;
+  const fabricMatrix = {};
+  for (let claim of claims) {
+    let [id, x, y, dx, dy] = claim;
+    for (let a of range(x, x + dx)) {
+      for (let b of range(y, y + dy)) {
+        let key = `${a}-${b}`;
+        if (!fabricMatrix[key]) fabricMatrix[key] = 0;
+        fabricMatrix[key] += 1;
+      }
+    }
+  }
+
+  return Object.values(fabricMatrix).reduce((a, x) => {
+    if (x > 1) a++;
+    return a;
+  }, 0);
+};
+
+const part2 = input => {
+  const claims = parseFile(input);
+  const fabricMatrix = {};
+  const noOverlap = new Set();
+
+  for (let claim of claims) {
+    let [id, x, y, dx, dy] = claim;
+    noOverlap.add(id);
+
+    for (let a of range(x, x + dx)) {
+      for (let b of range(y, y + dy)) {
+        let key = `${a}-${b}`;
+
+        if (!fabricMatrix[key]) {
+          fabricMatrix[key] = id;
+        } else {
+          noOverlap.delete(id);
+          noOverlap.delete(fabricMatrix[key]);
+        }
+      }
+    }
+  }
+
+  return noOverlap.values().next().value;
+};
+
+module.exports = { part1, part2 };
---
+++ day03/test.js
@@ -0,0 +1,15 @@
+const { part1, part2 } = require('./solution');
+
+const mockInput = `#1 @ 1,3: 4x4
+#2 @ 3,1: 4x4
+#3 @ 5,5: 2x2`;
+
+describe('Day 2', () => {
+  test('Part1', () => {
+    expect(part1(mockInput)).toBe(4);
+  });
+
+  test('Part2', () => {
+    expect(part2(mockInput)).toBe(3);
+  });
+});
